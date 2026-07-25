44cdd9318da7bfc768f06f5199fc211ff537cab8
eo <32405350+heyoeyo@users.noreply.github.com>
2026-07-25 11:14:44 -0400

    Minor update to 'close helper' to handle edge cases better - Specifically added fix for closing window 'b' in: [a [b c]] layout

--- niri_close_helper.sh
+++ niri_close_helper.sh
@@ -10,6 +10,8 @@ if [[ $# -gt 0 ]]; then
 	fi
 fi
 
+# ----- Handle special cases -----
+
 # Bail if we don't have a focused window
 # -> Will also happen when overview is open, so try to close anyways
 CURR_WIN_INFO=$(niri msg -j focused-window)
@@ -28,23 +30,30 @@ if $IS_FLOATING || [[ $WSPACE_ID == "null" ]]; then
 	exit
 fi
 
-# Close normally if we're in the first or second column (no point in forcing focus changes)
-# -> Doing this for the second column is important for preserving 'always-center-single-column' behavior
+# If we're in the first column, close normally (nothing else to do)
 COL_IDX=$(jq .layout.pos_in_scrolling_layout[0] <<< $CURR_WIN_INFO)
-if [[ $COL_IDX -lt 3 ]]; then
+if [[ $COL_IDX -eq 1 ]]; then
 	niri msg action close-window --id $WINID_TO_CLOSE
 	exit
 fi
 
-# Get all window info in current workspace
+# If we're in a stacked column, close normally (let niri handle focus change within column)
 ALL_WIN_INFO=$(niri msg -j windows | jq --argjson wsid $WSPACE_ID 'map(select(.workspace_id == $wsid))')
-
-# If we're in a stacked column, just close normally (let niri handle focus change)
 NUM_ROWS_IN_COL=$(jq --argjson cidx $COL_IDX 'map(select(.layout.pos_in_scrolling_layout[0] == $cidx)) | length' <<< $ALL_WIN_INFO)
 if [[ $NUM_ROWS_IN_COL -gt 1 ]]; then
 	niri msg action close-window --id $WINID_TO_CLOSE
 	exit
 fi
+
+# If there are exactly 2 (un-stacked) windows, close normally
+# -> Odd detail, but needed to preserve 'always-center-single-column' behavior
+NUM_WINS=$(jq length <<< $ALL_WIN_INFO)
+if [[ $NUM_WINS -eq 2 ]]; then
+	niri msg action close-window --id $WINID_TO_CLOSE
+	exit
+fi
+
+# ----- End of special cases -----
 
 # Figure out which window (left or right) to focus after closing
 LAST_COL_IDX=$(jq '[.[].layout.pos_in_scrolling_layout[0]] | max' <<< $ALL_WIN_INFO)
@@ -53,10 +62,9 @@ if $ENABLE_CLOSE_LEFT || [[ $COL_IDX -eq $LAST_COL_IDX ]]; then
 else
 	niri msg action focus-column-right
 fi
-SIDE_WIN_ID=$(niri msg -j focused-window | jq .id)
+AFTERCLOSE_WIN_ID=$(niri msg -j focused-window | jq .id)
 
 # Close and force view re-alignment to get rid of empty space on the right
 niri msg action close-window --id $WINID_TO_CLOSE
 niri msg action focus-column-first
-niri msg action focus-window --id $SIDE_WIN_ID
-
+niri msg action focus-window --id $AFTERCLOSE_WIN_ID
